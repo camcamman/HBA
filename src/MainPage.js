5d744cfe630ed58cdcb219d053335a42f4ec894d
Cameron Ford <c.r.ford@outlook.com>
2024-11-28 01:52:51 -0700

    added the transparent gray boxes with there buttons i added online continuing education button with working link. this link does not open in a new windo but it can easly be switched. also added the red click here for legislation update button with its link, that button will open the link in a new page. the whole header on the main page looks wise it pretty much done. it only works on computer

--- src/MainPage.js
+++ src/MainPage.js
@@ -6,7 +6,34 @@ const CustomNavbar = () => {
             <img class ="main-page-img-header"
             src="https://plus.unsplash.com/premium_photo-1671808062726-2a7ffcd6109e?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="house"/>
-            <button className="main-page-button" >How to get Licensed</button>   
+            <button className="how-to-get-licensed" >How to get Licensed</button>   
+
+            <div class="transparent-box-container">
+
+                <div class="transparent-box transparent-box-1">
+                    <p class="transparent-box-text">Webinar<br /> Continuing Education</p>
+                    <button class="enrroll-now-button">Enroll Now</button>
+                </div>
+                <div class="transparent-box transparent-box-2">
+                    <p class="transparent-box-text">In Person<br /> Continuing Education</p>
+                    <button class="enrroll-now-button">Enroll Now</button>
+                </div>
+                <div class="transparent-box transparent-box-3">
+                    <p class="transparent-box-text">Pre-License<br />Test Prep Courses</p>
+                    <button class="enrroll-now-button">Enroll Now</button>
+                </div>
+
+            </div>
+            <div class="online-continuing-education-container">
+                <button class="online-continuing-education" onClick={() => window.open("https://onlinece.hbautah.com/home/?site_template_id=60caa047fc128d640e5f0d25", '_self')}>Online Continuing Education</button>                
+            </div>
+            {/* <div class="online-continuing-education-container">
+                <button class="online-continuing-education" onClick={() => window.open("https://onlinece.hbautah.com/home/?site_template_id=60caa047fc128d640e5f0d25", '_blank')}>Online Continuing Education</button>                
+            </div> */}
+
+            <div class="click-daily-updates">
+                <button class="click-daily-updates-button" onClick={() => window.open("https://www.tiktok.com/@hbaofutah", '_blank')}>Click Here for the Daily Legislation Update</button>
+            </div>
         </div>
     );
 };
